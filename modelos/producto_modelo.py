7a1f87bcc1fe413a79e5e390fa57b017a6d30e50
Nicvolpe <130798898+Nicvolpe@users.noreply.github.com>
2024-07-14 16:33:02 -0300

    Add files via upload

---
+++ modelos/producto_modelo.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+
+from app import app, db          #,ma
+
+
+# defino las tablas
+class Producto(db.Model):   # la clase Producto hereda de db.Model    
+    id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
+    nombre=db.Column(db.String(100))
+    precio=db.Column(db.Integer)
+    stock=db.Column(db.Integer)
+    tipo=db.Column(db.String(50))
+    imagen=db.Column(db.String(400))
+    #tipoproducto = db.relationship('tipoproducto', backref='funciones')
+    def __init__(self,nombre,precio,stock,tipo,imagen):   #crea el  constructor de la clase
+        self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+        self.precio=precio
+        self.stock=stock
+        self.tipo=tipo
+        self.imagen=imagen
+
+
+with app.app_context():
+    db.create_all()  # aqui crea todas las tablas
+#  ************************************************************
